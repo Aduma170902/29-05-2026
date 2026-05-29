b8a71a5ee6dc125ae3a2ebb3811201ab319d7242
Aduma170902 <ianaduma69@gmail.com>
2026-05-29 14:24:06 -0400

    Primer commit

---
+++ computadorita/src/main/java/com/aduma/computadorita/ComputadoritaApplication.java
@@ -0,0 +1,13 @@
+package com.aduma.computadorita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputadoritaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputadoritaApplication.class, args);
+	}
+
+}
